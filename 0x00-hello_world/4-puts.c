d7713be1168c11778a2d78d2e24ca9ebd55ff938
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-16 00:22:16 -0700

    Taha Laaouini

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void)/*My fist code C in shell mode*/
+/**
+ * main
+ * Printing with comand puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

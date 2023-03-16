5261309c3bab10b1288aad1966a4e7d5a97f4629
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-16 01:22:18 -0700

    Taha Laaouini

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int number;
 long int longnumber;
 long long int longlongnumber;
 float numberwithfasila;
-printf("Size of a char: %lu byte(s)\n",sizeof(lettre));
-printf("Size of an int: %lu byte(s)\n",sizeof(number));
-printf("Size of a long int: %lu byte(s)\n",sizeof(longnumber));
-printf("Size of a long long int: %lu byte(s)\n",sizeof(longlongnumber));
-printf("Size of a float: %lu byte(s)\n",sizeof(numberwithfasila));
+printf("Size of a char: %lu byte(s)\n", sizeof(lettre));
+printf("Size of an int: %lu byte(s)\n", sizeof(number));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longnumber));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongnumber));
+printf("Size of a float: %lu byte(s)\n", sizeof(numberwithfasila));
 return (0);
 }

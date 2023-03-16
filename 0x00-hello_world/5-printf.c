52dce008b7efb57ba8ee639207ae17a86330660e
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-16 00:45:02 -0700

    Taha Laaouini

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a message to the console
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+/*Puts is designed to print strings to the console*/
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

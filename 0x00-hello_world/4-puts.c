9529378c73b599205217d2d0a4ea84a0e578dde0
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-16 00:30:32 -0700

    Taha Laaouini

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 /*Prints a message to the console*/
 int main(void)
 {
+/*Puts is designed to print strings to the console*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

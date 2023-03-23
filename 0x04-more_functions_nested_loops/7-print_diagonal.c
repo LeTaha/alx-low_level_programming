b7fa6c3f4cdf98fe2ea7ff1600b63e56e51dd571
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-23 15:50:23 -0700

    Laaouini Taha

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_diagonal - a function that draws a diagonal line
  * @n: An input integer
@@ -7,20 +8,18 @@
 void print_diagonal(int n)
 {
 int i = 0, j;
+
 if (n > 0)
 {
 for (; i < n; i++)
 {
 for (j = 0; j < i; j++)
-{
 _putchar(' ');
 _putchar(92);
 _putchar('\n');
 }
 }
-}
 else
-{
 _putchar('\n');
 }
-}
+

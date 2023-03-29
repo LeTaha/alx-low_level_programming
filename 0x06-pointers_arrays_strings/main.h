4fe7b24e2c56a1bf7e478fc8706b43871aaaf4e9
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-29 13:14:55 -0700

    Laaouini Taha

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,8 +20,13 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
+
 char *_strcat(char *dest, char *src);
+
+char *_strncpy(char *dest, char *src, int n);
+
 char *_strncat(char *dest, char *src, int n);
+
 int _atoi(char *s);
 
 int _putchar(char c);

481373f85b1ed5b889a9a23f951b8df748fb634c
Taha Laaouini <laaouini.taha@gmail.com>
2023-03-29 06:52:48 -0700

    LeTaha

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s1[98] = "Hello ";
+char s2[] = "World!\n";
+printf("%s\n", s1);
+printf("%s", s2);
+_strcat(s1, s2);
+printf("%s", s1);
+printf("%s", s2);
+return (0);
+}

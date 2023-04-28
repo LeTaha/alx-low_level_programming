60702df9f745533d4f96159f7a249f22c75572f8
Taha Laaouini <laaouini.taha@gmail.com>
2023-04-27 18:03:17 -0700

    Laaouini Taha

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,19 +11,19 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t str_len = 0;
+	list_t *new_nodes;
+	size_t str_lens = 0;
 
 	/*set strig length to 0 if its NULL*/
 	if (str == NULL)
-		str_len = 0;
+		str_lens = 0;
 
 	/*count length of string*/
-	while (str[str_len] != '\0')
-		str_len++;
+	while (str[str_lens] != '\0')
+		str_lens++;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new_nodes = malloc(sizeof(list_t));
+	if (new_nodes == NULL)
 		return (NULL);
 
 	/**
@@ -34,13 +34,13 @@ list_t *add_node(list_t **head, const char *str)
 	 *  of the list
 	 */
 	if (*head == NULL)
-		new_node->next = NULL;
+		new_nodes->next = NULL;
 	else
-		new_node->next = *head;
+		new_nodes->next = *head;
 
-	new_node->str = strdup(str);
-	new_node->len = str_len;
-	*head = new_node;
+	new_nodes->str = strdup(str);
+	new_nodes->len = str_lens;
+	*head = new_nodes;
 
 	return (*head);
 }
